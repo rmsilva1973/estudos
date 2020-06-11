e1eef5c0255c18dfc374119b8050976b0214f3fa
Renato Moutinho Silva <renato.moutinho@gmail.com>
2020-06-11 20:03:10 -0300

    NodeJS lesson finished

--- node-api/server.js
+++ node-api/server.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const cors = require('cors')
+const mongoose = require('mongoose')
+const requireDir = require('require-dir')
 
+// Iniciando o mongoose
 const app = express();
+app.use(express.json())
+app.use(cors())
 
-app.get('/', (req, res) => {
-    res.send('Oi! Tudo bem?')
-})
+// Iniciando o banco de dados
+mongoose.connect('mongodb://localhost:27017/node-api',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+);
+requireDir('./src/models');
+
+// Criando as rotas
+app.use('/api', require('./src/routes'))
+
 app.listen(3001);
